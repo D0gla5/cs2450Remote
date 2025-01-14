32794d07c2bc7692967163343677267eab48580f
Douglas <pomareleungchoi@gmail.com>
2025-01-14 15:31:12 -0700

    fixed repeated age asking

--- guesser.py
+++ guesser.py
@@ -3,12 +3,17 @@ import random
 print ("Hi! I'm going to try to guess your age.")
 
 guessed = False
+guesses = []
 while(guessed == False):
     guess = random.randint(15, 30)
-    print ("I guess you are", guess, "years old.")
+    if guess in guesses:
+        continue
+    
+    print ("Are you", guess, "years old.")
     response = input("Am I right? (y, n): ")
     if response == "y":
         guessed = True
         print("Yay!!")
     else:
         print ("Rats.")
+        guesses.append(guess)
