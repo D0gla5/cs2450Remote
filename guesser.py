ed417a8d7f453c2b0e431935ace2e917bcaa73d8
Douglas <pomareleungchoi@gmail.com>
2025-01-14 19:02:37 -0700

    added code that ends program after program guesses all numbers between 15 and 30

--- guesser.py
+++ guesser.py
@@ -6,9 +6,12 @@ guessed = False
 guesses = []
 while(guessed == False):
     guess = random.randint(15, 30)
+    if all(num in guesses for num in range(15, 31)):
+        print("Okay, I give up.")
+        break
     if guess in guesses:
         continue
-    
+
     print ("Are you", guess, "years old.")
     response = input("Am I right? (y, n): ")
     if response == "y":
@@ -17,3 +20,4 @@ while(guessed == False):
     else:
         print ("Rats.")
         guesses.append(guess)
+
